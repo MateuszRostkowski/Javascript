dfff9b84ef57f1bdc10c5b6ed4c0065bbb0eee5a
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2019-08-11 13:10:00 +0200

    3.  Basic Javascript Syntax

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
+
+// the first alert
 alert("Hello, World!");
+
+// Single line comment
+
+/* Multi
+Line
+Comment */
+
+
+// the second alert
+alert("Hello, World!");
+
+
+// Javascript runs from top to bottom
