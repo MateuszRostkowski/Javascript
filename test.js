3abd16c81cdc416cb47cdd1663163d8f9612d2b8
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2019-08-12 11:00:44 +0200

    8. For loops

--- test.js
+++ test.js
@@ -1,11 +1,13 @@
-var age = 5;
 
-while (age < 10) {
 
-    console.log("Your age is less than 10");
-    age++;
-    console.log("Your age is:" + age);
+var links = document.getElementsByTagName("a");
+
+
+for (i = 1; i <= links.length; i++) {
+
+    console.log("this is link number" + i);
 
 }
 
-document.write("you are now over 10");
+
+document.write("all links now looped");
