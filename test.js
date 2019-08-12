9b812ef97d7913bbd9398c8d55a7c8bc65e73712
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2019-08-12 11:30:57 +0200

    12. Variable Scope - Global and Local

--- test.js
+++ test.js
@@ -1,13 +1,29 @@
-function getAverage(a,b) {
 
-    var average = (a + b) / 2;
+//Global variable
+var foo = 20;
+
+
+// Local variable
+// It can't be call outside of the function
+function myFunction() {
+    var bar  = 10;
+}
+
+
+function getAverage (a,b) {
+
+    var average = (a + b) / 2; //local variable
     console.log(average);
     return average;
 
 }
 
+var myResult = getAverage(7, 11); //global variable
 
-var myResult = getAverage(7,8);
-getAverage(5,3);
-getAverage(20,10);
-console.log(myResult);
+function logResult() {
+
+    console.log("the average is: " + myResult + " inside the function");
+
+}
+
+logResult();
