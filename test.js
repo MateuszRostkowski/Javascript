82bbb0df58e49cc8a51ffccecd05be91b96ed3a7
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2019-08-12 11:22:17 +0200

    11. Js functions

--- test.js
+++ test.js
@@ -1,7 +1,13 @@
-var links  = document.getElementsByTagName("a");
+function getAverage(a,b) {
 
-for(i = 0; i < links.length; i++) {
-
-    links[i].className = "Link-" + i;
+    var average = (a + b) / 2;
+    console.log(average);
+    return average;
 
 }
+
+
+var myResult = getAverage(7,8);
+getAverage(5,3);
+getAverage(20,10);
+console.log(myResult);
