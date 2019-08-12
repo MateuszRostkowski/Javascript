877ba16034b90b40365a4f39ca53f72b436103ec
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2019-08-12 10:45:35 +0200

    6. Logical Operators

--- test.js
+++ test.js
@@ -1,12 +1,8 @@
-var myAge = 19;
+var myAge = 26;
 
-if (myAge > 30) {
-    document.write("you are over 30!");
-} else if (myAge > 20) {
-    document.write("you are over 20!");
-} else if (myAge > 10) {
-    document.write("you are over 10!");
+if (myAge < 18 || myAge > 30 || myAge === 25) {
+    document.write("you aint comming!")
+
 } else {
-    document.write("you are not over 10!");
+    document.write("You can come, you are cool dude")
 }
-
