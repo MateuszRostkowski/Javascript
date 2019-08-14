ea08b1f6f7f6af69aa43846ae57b8549b2e31f37
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2019-08-14 16:37:20 +0200

    16. This keyword

--- test.js
+++ test.js
@@ -7,23 +7,30 @@ var myCar = new Object();
 myCar.maxSpeed = 50;
 myCar.driver  = "Shaun";
 myCar.drive = function(speed, time) {
-    console.log("The car is driving now. The driver is " + myCar.driver + ". With the maxspeed of " + myCar.maxSpeed + "km/h. And he drive: " + speed * time + " kilometers.");
+    console.log("The car is driving now. The driver is " + myCar.driver + ". With the maxspeed of " + myCar.maxSpeed + "km/h. And he traveled " + speed * time + " kilometers.");
 };
 
 myCar.drive(10,10);
 
-
-var myArray2 = [true, 15, "hi"];
 
 
 // it's same as object myCar but in shorter version!!
 var myCar2 = {
     maxSpeed: 50,
     driver: "Mateusz",
-    drive: (speed, time) => {
+    drive: function(speed, time) {
         console.log(speed * time);
-    }
+        console.log(
+            "The car is driving now. The driver is " +
+            this.driver +
+            ", with the masxspeed of " +
+            this.maxSpeed +
+            "km/h. And he traveled "
+            + speed * time +
+            " kilometers."
+        );
+    },
 };
 
-console.log(myCar2.maxSpeed);
+
 myCar2.drive(50, 3);
