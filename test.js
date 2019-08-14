c420d97d2082faad31a106fed30c22109fb329b1
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2019-08-14 12:17:23 +0200

    15.1 Update to creating new Javascript objects

--- test.js
+++ test.js
@@ -2,18 +2,21 @@ var myArray = new Array()
 myArray[0] = 8;
 myArray[1] = "hello";
 
+// this is not the best method to create Objects
 var myCar = new Object();
 myCar.maxSpeed = 50;
 myCar.driver  = "Shaun";
-myCar.drive = function() {
-    console.log("now driving");
+myCar.drive = function(speed, time) {
+    console.log("The car is driving now. The driver is " + myCar.driver + ". With the maxspeed of " + myCar.maxSpeed + "km/h. And he drive: " + speed * time + " kilometers.");
 };
 
-myCar.drive();
+myCar.drive(10,10);
 
 
 var myArray2 = [true, 15, "hi"];
 
+
+// it's same as object myCar but in shorter version!!
 var myCar2 = {
     maxSpeed: 50,
     driver: "Mateusz",
